5ace37503e1d69f718aef5df6fa307a1148de3df
adjansxzd <aka48402@gmail.com>
2025-02-11 01:18:39 +0500

    commit

---
+++ LAB4/math/parallelogram.py
@@ -0,0 +1,6 @@
+def parallelogram(b, h):
+    return b * h
+
+base = float(input("Length of base: "))
+height = float(input("Height of parallelogram: "))
+print("Expected Output: ", parallelogram(base, height))
